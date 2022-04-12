004a4f540e961cdbf5ed795516c4d1debc5ebe39
alizer5 <93188190+alizer5@users.noreply.github.com>
2022-04-12 15:49:04 +0500

    Login Page for Validation

---
+++ login.js
@@ -0,0 +1,10 @@
+const username=document.getElementById('username')
+const password=document.getElementById('password')
+const formid=document.getElementById('form1')
+formid.addEventListener('submit',(e)=>
+{ 
+    let messages=[]
+   
+    
+
+})

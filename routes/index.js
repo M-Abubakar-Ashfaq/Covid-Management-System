20e6740ca73988fa8d5aee7afa098fecc441ae72
M-Abubakar-Ashfaq <90302310+M-Abubakar-Ashfaq@users.noreply.github.com>
2022-05-30 00:41:19 +0500

    routes updated

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
+
 // Welcome Page
+
+
+
 router.get('/', forwardAuthenticated, (req, res) => res.render('home'));
 
 router.get('/views/welcome',(req, res) => res.render('welcome'));
@@ -13,9 +17,39 @@ router.get('/login', ensureAuthenticated,(req,res)=>
 router.get('/doctor-add', ensureAuthenticated,(req,res)=>
   res.render('doctor-add'));
 
+  router.get('/doctor',(req,res)=>{
+    res.render('doctor.ejs');
+}
+)
+router.get('/doctor-delete', ensureAuthenticated,(req,res)=>{
+    res.render('doctor-delete.ejs')
+}
+)
+router.get('/doctor-display', ensureAuthenticated,(req,res)=>{
+    res.render('doctor-display.ejs')
+}
+)
+router.get('/doctor-update', ensureAuthenticated,(req,res)=>{
+    res.render('doctor-update.ejs')
+}
+)
+
   router.get('/patient-add', ensureAuthenticated,(req,res)=>
   res.render('patient-add'));
-// Dashboard
+
+  router.get('/patient-delete', ensureAuthenticated,(req,res)=>
+  res.render('patient-delete'));
+
+  router.get('/patient-display', ensureAuthenticated,(req,res)=>
+  res.render('patient-display'));
+
+  router.get('/patient-update', ensureAuthenticated,(req,res)=>
+  res.render('patient-update'));
+
+
+
+ 
+
 
 
 
